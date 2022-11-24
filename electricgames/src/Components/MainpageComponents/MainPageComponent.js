cee8ffb8940f9a6a1da1a1460a0a8151e5e81aef
JTHaugseth <97683128+JTHaugseth@users.noreply.github.com>
2022-11-24 20:49:58 +0100

    fwafwa

--- electricgames/src/Components/MainpageComponents/MainPageComponent.js
+++ electricgames/src/Components/MainpageComponents/MainPageComponent.js
@@ -13,13 +13,12 @@ export default class MainPageComponent extends Component {
             <p>Ahead of the curve with 200+ active projects.<br/>
             Currently employed by Kristiania, Bergen.</p>
            
-            <div className="buttons">
-            <div className="row justify-content-center">
-            <Link to="/GamesCollection"><button id="gm-btn" className="col col-md-12">Game Collection</button></Link>    
-            <Link to="/CharactersCollection"><button id="chr-btn" className="col col-md-12 ">Character Collection</button></Link>    
-            <Link to="/Quiz"><button id="quiz-btn" className="col col-md-12">Quiz Gamepage</button></Link>  
+            
+            <div id="mainbuttons" className="row justify-content-center g-2">
+            <Link to="/GamesCollection"><button id="mainbtn" className="col-lg-8 col-md-10 col-sm-12">Game Collection</button></Link>    
+            <Link to="/CharactersCollection"><button id="mainbtn" className="col-lg-8 col-md-10 col-sm-12">Character Collection</button></Link>    
+            <Link to="/Quiz"><button id="mainbtn" className="col-lg-8 col-md-10 col-sm-12">Quiz Gamepage</button></Link>  
                     </div>  
-                </div>
             </div>
          </div> 
         );
